e065dbd69ae738a2e262448265db78ae7f323ea3
Alexandre Saelens <alexandre.saelens@dna.co.nz>
2023-10-16 14:46:51 +1300

    Update dependecies

--- src/DataObjects/YoutubeEmbed.php
+++ src/DataObjects/YoutubeEmbed.php
@@ -2,13 +2,13 @@
 
 namespace DNADesign\YoutubeEmbed;
 
+use Detection\MobileDetect;
+use EdgarIndustries\YouTubeField\YouTubeField;
+use SilverStripe\AssetAdmin\Forms\UploadField;
+use SilverStripe\Assets\Image;
+use SilverStripe\Forms\TextField;
 use SilverStripe\ORM\DataObject;
 use SilverStripe\View\ArrayData;
-use SilverStripe\Assets\Image;
-use SilverStripe\AssetAdmin\Forms\UploadField;
-use EdgarIndustries\YouTubeField\YouTubeField;
-use SilverStripe\Forms\TextField;
-use Sinergi\BrowserDetector\Os;
 
 class YoutubeEmbed extends DataObject
 {
@@ -29,8 +29,8 @@ class YoutubeEmbed extends DataObject
 
     public function getIsMobile()
     {
-        $os = new Os();
-        return $os->getIsMobile();
+        $detect = new MobileDetect;
+        return $detect->isMobile();
     }
 
     public function getCMSFields()
